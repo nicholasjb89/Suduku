017221a1aa80aae6bd2c7d0aaee817156541b09d
nicholasjb89 <nicholasjbailey@hotmail.com>
2016-05-01 16:05:58 -0400

    got it working a little bit.
    It can only solve the simplest of sudokus.

--- build.py
+++ build.py
@@ -2,40 +2,31 @@ import board
 import random
 import copy
 
-FULLBOARD = board.buildBoard()
-GAMEBOARD = copy.copy(FULLBOARD)
-TEMPREMOVED = [0,0] # index, number
-REMAINING = list(range(0,81))
-REMOVED_INDEXES = []
-
-def numberRemove():
+def numberRemove(tempRemoved, remaining, removedIndexes, gameBoard):
     """removes a number from gameboard"""
+    tempRemoved[0] = random.choice(remaining)
+    tempRemoved[1] = gameBoard[tempRemoved[0]]
+    remaining.remove(tempRemoved[0])
+    removedIndexes.append(tempRemoved[0])
+    gameBoard[tempRemoved[0]] = 0 # replace the number with an empty string
 
-    TEMPREMOVED[0] = random.choice(REMAINING)
-    TEMPREMOVED[1] = FULLBOARD[TEMPREMOVED[0]]
-    REMAINING.remove(TEMPREMOVED[0])
-    REMOVED_INDEXES.append(TEMPREMOVED[0])
-    GAMEBOARD[TEMPREMOVED[0]] = 0 # replace the number with an empty string
+    return tempRemoved
 
 
-def undoRemove():
+def undoRemove(tempRemoved, remaining, removedIndexes, gameboard):
     """undo's the remove"""
-    global TEMPREMOVED
+    remaining.append(tempRemoved[0])
+    gameboard[tempRemoved[0]] = tempRemoved[1]
+    removedIndexes.remove(tempRemoved[0])
 
-    REMAINING.append(TEMPREMOVED[0])
-    GAMEBOARD[TEMPREMOVED[0]] = TEMPREMOVED[1]
-    REMOVED_INDEXES.remove(TEMPREMOVED[0])
-    TEMPREMOVED = [0,0] # reset tempremove
-
-
-def solvable():
+def solvable(gameBoard, removedIndexes):
     """returns true if it is solvable"""
-    tempBoard = copy.copy(GAMEBOARD)
-    tempRemoved = copy.copy(REMOVED_INDEXES)
+    tempBoard = copy.copy(gameBoard)
+    tempRemoved = copy.copy(removedIndexes)
     f_solvable = False
 
     while True:
-        for index in REMOVED_INDEXES:
+        for index in tempRemoved:
             col = board.getColNumbers(index,tempBoard)
             row = board.getRowNumbers(index,tempBoard)
             square = board.getSquareNumbers(index,tempBoard)
@@ -44,54 +35,59 @@ def solvable():
             for n in col:
                 if n in row and n in square:
                     validNumbers.append(n)
-
+            print(index,validNumbers)
             if len(validNumbers) == 1:
+
                 tempBoard[index] == validNumbers[0]
                 tempRemoved.remove(index)
                 f_solvable = True
 
         if f_solvable == False:
+            print(tempBoard)
             return False
         elif len(tempRemoved) == 0:
             return True
 
         f_solvable = False
 
-#remove random numbers
+def createGameBoard(removedAmount):
+    fullBoard = board.buildBoard()
+    gameBoard = copy.copy(fullBoard)
+    remaining = list(range(0,81))
+    removedIndexes = []
+    tempRemoved = [0,0]
 
-numberRemove()
-numberRemove()
-numberRemove()
-numberRemove()
-numberRemove()
-numberRemove()
-numberRemove()
-numberRemove()
+    for i in range(1000):
+        tempRemoved = numberRemove(tempRemoved, remaining, removedIndexes, gameBoard)
+        if not solvable(gameBoard,removedIndexes):
+            undoRemove(tempRemoved,remaining,removedIndexes,gameBoard)
+        if len(removedIndexes) == removedAmount:
+            break
 
-while True:
-    row = []
-    index = 0
-    for num in GAMEBOARD:
-        row.append(num)
-        if index == 8:
-            print(row)
-            row = []
-            index = -1
+    return gameBoard,fullBoard
 
-        index += 1
-    break
+def solveGameBoard(gameBoard):
+    removedIndexes = []
+    for i in range(81):
+        if gameBoard[i] == 0:
+            removedIndexes.append(i)
 
-print(solvable())
+    print(len(removedIndexes))
 
-
+    solvable(gameBoard, removedIndexes)
 
 
+GAMEBOARD = [0,2,0,1,7,8,0,3,0,
+             0,4,0,3,0,2,0,9,0,
+             1,0,0,0,0,0,0,0,6,
+             0,0,8,6,0,3,5,0,0,
+             3,0,0,0,0,0,0,0,4,
+             0,0,6,7,0,9,2,0,0,
+             9,0,0,0,0,0,0,0,2,
+             0,8,0,9,0,1,0,6,0,
+             0,1,0,4,3,6,0,5,0]
 
-
-
-
-
-
+solveGameBoard(GAMEBOARD)
 
 
 
