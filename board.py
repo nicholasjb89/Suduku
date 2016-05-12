cbb8156dcbf6e4b3fd6eb48c1b677ca684def9a1
nicholasjb89 <nicholasjbailey@hotmail.com>
2016-05-12 13:47:50 -0400

    A commit before i fuck shit up
    Rewriting some of the code so creating this backup incase it goes crazy.

--- board.py
+++ board.py
@@ -1,58 +1,150 @@
 import random
 import copy
 
-def getSquareIndexes(index,board):
-    squareIndexes = (0,1,2,9,10,11,18,19,20),\
-                    (3,4,5,12,13,14,21,22,23),\
-                    (6,7,8,15,16,17,24,25,26),\
-                    (27,28,29,36,37,38,45,46,47),\
-                    (30,31,32,39,40,41,48,49,50),\
-                    (33,34,35,42,43,44,51,52,53),\
-                    (54,55,56,63,64,65,72,73,74),\
-                    (57,58,59,66,67,68,75,76,77),\
-                    (60,61,62,69,70,71,78,79,80)
-    indexes = []
-    for square in squareIndexes:
-        if index in square:
-            for i in square:
-                if board[index] == 0:
+def displayBoard(board):
+    print()
+    row = []
+    col = 1
+    for n in board:
+        if len(row) != 9:
+            row.append(n)
+        elif len(row) == 9:
+            for i in range(0,9):
+                if i%3 == 0:
+                    print(" |",end="")
+                print(row[i], end=" ")
+            row = []
+            row.append(n)
+            print()
+            if col %3 == 0:
+                print(" -----------------------")
+            col += 1
+    for i in range(0,9):
+        if i%3 == 0:
+            print(" |",end="")
+        print(row[i], end=" ")
+    print()
+
+class Validate():
+    def getSquareIndexes(self,index):
+        squareIndexes = (0,1,2,9,10,11,18,19,20),\
+                        (3,4,5,12,13,14,21,22,23),\
+                        (6,7,8,15,16,17,24,25,26),\
+                        (27,28,29,36,37,38,45,46,47),\
+                        (30,31,32,39,40,41,48,49,50),\
+                        (33,34,35,42,43,44,51,52,53),\
+                        (54,55,56,63,64,65,72,73,74),\
+                        (57,58,59,66,67,68,75,76,77),\
+                        (60,61,62,69,70,71,78,79,80)
+        indexes = []
+        for square in squareIndexes:
+            if index in square:
+                for i in square:
                     indexes.append(i)
-    return indexes
+        return indexes
 
-def getRowIndexes(index,board):
-    indexes = []
+    def getRowIndexes(self, index):
+        indexes = []
 
-    offset = index%9
-    index -= offset
+        offset = index%9
+        index -= offset
 
-    for i in range(index,index+9):
-        if board[i] == 0:
+        for i in range(index,index+9):
             indexes.append(i)
 
-    return indexes
+        return indexes
 
-def getColIndexes(index,board):
-    indexes = []
-    while True:
-    #start at the begining of the collumn
-        if index - 9 > 0:
-            index -= 9
-        else:
-            break
-    while True:
-        if board[index] == 0:
+    def getColIndexes(self,index):
+        indexes = []
+        while True:
+        #start at the begining of the collumn
+            if index - 9 >= 0:
+                index -= 9
+            else:
+                break
+        while True:
             indexes.append(index)
-        index += 9
-        if index >=81:
-            break
+            index += 9
 
-    return indexes
+            if index >= 81:
+                break
+
+        return indexes
+
+    def isRepeated(self, number, indexes, board):
+        count = 0
+        for i in indexes:
+            if number == board[i]:
+                count += 1
+
+        if count > 1:
+            return True
+        else:
+            return False
+
+    def validateBoard(self,board):
+        f_missingIndex = False
+        missingIndexPositions = []
+        f_repeatedNumber = False
+        repeatedNumbers = []
+        for i in range(0,81):
+            n = board[i]
+            indexNumPair = (i,n)
+            row = self.getRowIndexes(i)
+            col = self.getColIndexes(i)
+            square = self.getSquareIndexes(i)
+            if n == 0:
+                f_missingIndex = True
+                missingIndexPositions.append(i)
+
+            if self.isRepeated(n, row, board):
+                f_repeatedNumber = True
+                if indexNumPair not in repeatedNumbers:
+                    repeatedNumbers.append(indexNumPair)
+            if self.isRepeated(n, col, board):
+                f_repeatedNumber = True
+                if indexNumPair not in repeatedNumbers:
+                    repeatedNumbers.append(indexNumPair)
+            if self.isRepeated(n, square, board):
+                f_repeatedNumber = True
+                if indexNumPair not in repeatedNumbers:
+                    repeatedNumbers.append(indexNumPair)
+
+        return f_missingIndex, missingIndexPositions, f_repeatedNumber, repeatedNumbers
+
+    def validate(self,board, errorLog = False):
+        f_missingIndex, missingIndexPositions, f_repeatedNumber, repeatedNumbers = self.validateBoard(board)
+
+        if f_missingIndex and errorLog:
+            print("There are missing boxes in the board at these positions")
+            print(missingIndexPositions)
+        if f_repeatedNumber and errorLog:
+            print()
+            print("There are repeated numbers in the board")
+            failBoard = []
+            rIndex = 0
+
+            for i in range(0,81):
+                if rIndex < len(repeatedNumbers):
+                    if i == repeatedNumbers[rIndex][0]:
+                        failBoard.append(repeatedNumbers[rIndex][1])
+                        rIndex += 1
+                    else:
+                        failBoard.append(" ")
+                else:
+                        failBoard.append(" ")
+            displayBoard(failBoard)
+
+        if f_repeatedNumber or f_missingIndex:
+            return False
+        else:
+            return True
 
 class Solve():
-    def __init__(self):
-        pass
+    def __init__(self,validateClass):
+        self.validate = validateClass
 
-    def solve(self,board):
+    def alg1(self, board):
         missingIndexes = self.getMissingIndexes(board)
 
         f_solvable = False
@@ -78,6 +170,47 @@ class Solve():
 
             f_solvable = False
 
+    def alg2(self,board):
+        pass
+
+    def charleyAlg(self,board):
+        """this is charleys brute force alg"""
+
+        for i in self.getMissingIndexes(board):
+            row = self.getRowNumbers(i,board)
+            col = self.getColNumbers(i,board)
+            square = self.getSquareNumbers(i,board)
+            validNumbers = []
+            for n in row:
+                if n in col and n in square:
+                    validNumbers.append(n)
+            try:
+                board[i] = random.choice(validNumbers)
+            except:
+                break
+
+        return board
+
+    def solve(self,board):
+        board = self.alg1(board)
+        bestBoard = board
+        "This is the Game Board before Brute Force"
+        if Validate().validate(board):
+            return board
+        else:
+            i = 0
+            while True:
+                i += 1
+                tempBoard = self.charleyAlg(copy.copy(board))
+                if len(self.getMissingIndexes(tempBoard)) < len(self.getMissingIndexes(bestBoard)):
+                    bestBoard = tempBoard
+
+                if(i%1000 == 0):
+                    print("attempts made: ",  i)
+                    displayBoard(bestBoard)
+                if Validate().validate(tempBoard):
+                    return tempBoard
+
     def getRowNumbers(self, index, board):
         validNumbers = list(range(1,10))
 
@@ -95,7 +228,7 @@ class Solve():
 
         while True:
             #start at the begining of the collumn
-            if index - 9 > 0:
+            if index - 9 >= 0:
                 index -= 9
             else:
                 break
@@ -122,7 +255,7 @@ class Solve():
                         (57,58,59,66,67,68,75,76,77),\
                         (60,61,62,69,70,71,78,79,80)
 
-        for square  in squareIndexes:
+        for square in squareIndexes:
             if index in square:
                 break
 
@@ -139,24 +272,19 @@ class Solve():
                 indexes.append(i)
         return indexes
 
-    def isValid(self, board):
-        if 0 in board:
-            return False
-        else:
-            return True
-
 class Board():
-    def __init__(self, solveClass):
-        self.solve = solveClass()
+    def __init__(self, solveClass, validateClass):
+        self.solver = solveClass(validateClass)
+        self.validate = validateClass()
         self.build()
 
     def attemptBuild(self):
         self.board = [0] * 81
         for i in range(81):
             tempBoard = self.getBoard()
-            rowNums = self.solve.getRowNumbers(i, tempBoard)
-            colNums = self.solve.getColNumbers(i, tempBoard)
-            squareNums = self.solve.getSquareNumbers(i, tempBoard)
+            rowNums = self.solver.getRowNumbers(i, tempBoard)
+            colNums = self.solver.getColNumbers(i, tempBoard)
+            squareNums = self.solver.getSquareNumbers(i, tempBoard)
 
             validNumbers = []
 
@@ -170,17 +298,15 @@ class Board():
                 break
 
     def build(self):
+        self.failed = 0
         while True:
             self.attemptBuild()
-            if self.solve.isValid(self.board):
+            if self.validate.validate(self.board):
                 break
+            self.failed += 1
 
     def getBoard(self):
         return copy.copy(self.board)
-
-if __name__ == "main":
-    pass
-
 
 
 
--- test.py
+++ test.py
@@ -1,6 +1,26 @@
 from board import *
 import unittest
 
+NOTVALIDBOARD = [7, 4, 5, 9, 6, 3, 8, 2, 1,
+                 3, 5, 7, 4, 1, 2, 6, 8, 9,
+                 8, 7, 2, 6, 4, 1, 9, 3, 5,
+                 4, 6, 1, 5, 9, 8, 2, 7, 3,
+                 9, 2, 6, 3, 8, 5, 4, 1, 7,
+                 5, 1, 3, 8, 2, 9, 7, 6, 4,
+                 2, 3, 4, 7, 5, 6, 1, 9, 8,
+                 1, 9, 8, 2, 3, 7, 5, 4, 6,
+                 6, 8, 9, 1, 7, 4, 3, 5, 2]
+
+VALIDBOARD = [9,3,6,8,1,7,4,5,2,
+            4,8,5,2,3,6,7,9,1,
+            7,2,1,4,9,5,6,8,3,
+            8,4,3,5,6,9,2,1,7,
+            2,1,7,3,4,8,9,6,5,
+            5,6,9,7,2,1,3,4,8,
+            3,5,8,6,7,4,1,2,9,
+            6,9,2,1,8,3,5,7,4,
+            1,7,4,9,5,2,8,3,6]
+
 EASYGAMEBOARD = [1,0,5,0,0,0,2,0,3,
                  0,3,8,7,5,6,4,1,0,
                  0,0,0,0,0,0,0,0,0,
@@ -41,20 +61,68 @@ MEDIUMSOLVEDGAMEBOARD = [6,3,7,8,5,1,2,9,4,
                          2,6,8,5,4,9,7,1,3,
                          3,4,5,7,1,2,6,8,9]
 
-class build_board(unittest.TestCase):
-    def test_build(self):
-        board = Board(Solve)
-        full = False
-        if 0 not in board.getBoard():
-            full = True
+WORLDHARDEST = [8,0,0,0,0,0,0,0,0,
+                0,0,3,6,0,0,0,0,0,
+                0,7,0,0,9,0,2,0,0,
+                0,5,0,0,0,7,0,0,0,
+                0,0,0,0,4,5,7,0,0,
+                0,0,0,1,0,0,0,3,0,
+                0,0,1,0,0,0,0,6,8,
+                0,0,8,5,0,0,0,1,0,
+                0,9,0,0,0,0,4,0,0]
 
-        self.assertEqual(full, True)
+SOLVEDWORLHARDEST = [8,1,2,7,5,3,6,4,9,
+                     9,4,3,6,8,2,1,7,5,
+                     6,7,5,4,9,1,2,8,3,
+                     1,5,4,2,3,7,8,9,6,
+                     3,6,9,8,4,5,7,2,1,
+                     2,8,7,1,6,9,5,3,4,
+                     5,2,1,9,7,4,3,6,8,
+                     4,3,8,5,2,6,9,1,7,
+                     7,9,6,3,1,8,4,5,2]
 
-class solve_board_easy(unittest.TestCase):
+class Build_board(unittest.TestCase):
+    def test_if_valid_board(self):
+        board = Board(Solve,Validate)
+        valid = Validate().validate(board.getBoard())
+        self.assertEqual(valid, True)
+
+    def test_failed_board_percent(self):
+        failed = 0
+        for i in range(0,25):
+            board = Board(Solve,Validate)
+            failed += board.failed
+        print("Average fail rate = ", failed/25)
+        self.assertLess(failed/25,500)
+
+class Solve_board_easy(unittest.TestCase):
     def test_solve(self):
-        solved = Solve().solve(EASYGAMEBOARD)
+        solved =Board(Solve,Validate).solver.solve(EASYGAMEBOARD)
         self.assertEqual(solved,EASYSOLVEDGAMEBOARD)
 
+class Solve_board_medium(unittest.TestCase):
+    def test_solve(self):
+        solved = Board(Solve,Validate).solver.solve(MEDIUMGAMEBOARD)
+        self.assertEqual(solved,MEDIUMSOLVEDGAMEBOARD)
+"""
+class solve_board_worldHardest(unittest.TestCase):
+    def test_solve(self):
+        solved = Board(Solve,Validate).solver.solve(WORLDHARDEST)
+        self.assertEqual(solved,SOLVEDWORLHARDEST,displayBoard())
+"""
+
+class Validate_board(unittest.TestCase):
+    def test_validate_pass(self):
+        board = EASYSOLVEDGAMEBOARD
+        valid = Validate().validate(board)
+
+        self.assertEqual(valid,True)
+
+    def test_validate_fail(self):
+        board = NOTVALIDBOARD
+        valid = Validate().validate(board,errorLog=True)
+
+        self.assertEqual(valid,False)
 
 if __name__ == "__main__":
     unittest.main()
